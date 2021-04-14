9903e08b50cf51c3680b960986a5b7c4dc95c86e
martin <martin@irm.kr>
2021-04-14 15:18:59 +0900

    heale admin ui2 완료

--- assets/js/healme_info.js
+++ assets/js/healme_info.js
@@ -1,5 +1,9 @@
 $(function () {
 
-    
+    $('.price_info li').on('click',function(){
+        var tg = "#" + $(this).attr("rel");
+        $(this).addClass('on').siblings('li').removeClass('on');
+        $(tg).addClass('on').siblings('div').removeClass('on');
+    });
     
 });
